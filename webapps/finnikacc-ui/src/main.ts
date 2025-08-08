6c012d02876e9edcf4e83b775850766e3beab5ea
ihor <Ihor.Halaniuk@gmail.com>
2025-08-08 18:19:09 +0300

    0. add gtag for google analytics in prod only.

--- webapps/finnikacc-ui/src/main.ts
+++ webapps/finnikacc-ui/src/main.ts
@@ -3,6 +3,8 @@ import './assets/style.css'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { configure } from 'vue-gtag'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -20,5 +22,11 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(vuetify)
+
+if (import.meta.env.PROD) {
+    configure({
+        tagId: 'G-PYN2E2SK7V',
+    })
+}
 
 app.mount('#app')
