07784f6dbfab710d1db9b67a566b6031d830a314
ihor <Ihor.Halaniuk@gmail.com>
2025-08-09 06:03:19 +0300

    0. Fix build.

--- webapps/finnikacc-ui/src/types-and-utils/useCurrentTime.ts
+++ webapps/finnikacc-ui/src/types-and-utils/useCurrentTime.ts
@@ -3,7 +3,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 export function useCurrentTime(updateIntevalMs: number = 15000) {
     const now = ref(Date.now())
 
-    let timerId: number | undefined
+    let timerId: ReturnType<typeof setInterval> | undefined
     onMounted(() => {
         timerId = setInterval(() => {
             now.value = Date.now()
