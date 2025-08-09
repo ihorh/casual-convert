cb3c7097a43c33bf232b0a96cd0ae5ede01b110b
ihor <Ihor.Halaniuk@gmail.com>
2025-08-09 15:02:33 +0300

    0. Fixed working dir for dev_local env.

--- src/finnikacc/scripts/api_server_dev.py
+++ src/finnikacc/scripts/api_server_dev.py
@@ -1,14 +1,16 @@
+import os
+
+os.environ["APP_ENV"] = "dev_local"
+
 import uvicorn
 
 
 def main() -> None:
+    os.chdir("./packages/finnikacc-api")
     uvicorn.run(
         "finnikacc_api.main:app",
         host="0.0.0.0",  # noqa: S104
         port=8000,
         log_level="info",
         reload=True,
-        reload_dirs=[
-            "packages/finnikacc-api",
-        ],
     )
