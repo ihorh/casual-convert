4d8e86b87ff2738b0b8b5eadc02ee70bb98d136a
ihor <Ihor.Halaniuk@gmail.com>
2025-08-15 11:43:14 +0300

    8. Display versions in the footer.

--- webapps/finnikacc-ui/src/layouts/Footer.vue
+++ webapps/finnikacc-ui/src/layouts/Footer.vue
@@ -11,5 +11,24 @@
                 Project GitHub Page
             </a>
         </div>
+        <div>|</div>
+        <div>App Version: {{ appVersion }}</div>
+        <div>|</div>
+        <div>API Version: {{ apiServerVersion }}</div>
     </div>
 </template>
+
+<script lang="ts" setup>
+import { useAPIServerStatus } from '@/stores/queries'
+import { computed } from 'vue'
+
+const appVersion = __APP_VERSION__
+
+const { isError, data: apiServerStatus, error } = useAPIServerStatus()
+
+const apiServerVersion = computed(() =>
+    apiServerStatus.value && apiServerStatus.value.version
+        ? apiServerStatus.value.version
+        : 'fetching...',
+)
+</script>
--- webapps/finnikacc-ui/src/views/CurrencyConverterView.vue
+++ webapps/finnikacc-ui/src/views/CurrencyConverterView.vue
@@ -17,23 +17,6 @@
         <div class="h-4"></div>
 
         <disclaimer />
-
-        <br />
-        <hr />
-        <br />
-
-        <div class="grid grid-cols-2 gap-4 w-max">
-            <div>Env Key</div>
-            <div>Value</div>
-            <div>version</div>
-            <div>{{ appVersion }}</div>
-            <div>API version</div>
-            <div>{{ apiServerVersion }}</div>
-            <template v-for="(value, key) in env">
-                <div>{{ key }}</div>
-                <div>{{ value }}</div>
-            </template>
-        </div>
     </DefaultLayout>
 </template>
 
@@ -42,25 +25,4 @@ import DefaultLayout from '@/layouts/DefaultLayout.vue'
 import CcBasket from '@/components/CcBasket.vue'
 import Disclaimer from '@/layouts/Disclaimer.vue'
 import Footer from '@/layouts/Footer.vue'
-import { useAPIServerStatus } from '@/stores/queries'
-import { computed } from 'vue'
-
-const ENV_MODE = import.meta.env.MODE
-const BASE_URL = import.meta.env.BASE_URL
-const PROD = import.meta.env.PROD
-const DEV = import.meta.env.DEV
-const SST = import.meta.env.SSR
-
-const env = import.meta.env
-const keys = Object.keys(import.meta.env)
-
-const appVersion = __APP_VERSION__
-
-const { isError, data: apiServerStatus, error } = useAPIServerStatus()
-
-const apiServerVersion = computed(() =>
-    apiServerStatus.value && apiServerStatus.value.version
-        ? apiServerStatus.value.version
-        : 'fetching...',
-)
 </script>
